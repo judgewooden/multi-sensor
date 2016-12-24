fa7a36d35e95bd211443085e66920f5dd42f0931
judgewooden <douwe.jong@gmail.com>
2016-12-24 02:18:48 +0100

    Latest Version

---
+++ startsensors.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# pkill -f mSensor
+
+
+python mSensor.py -c 11 &
+python mSensor.py -c 21 &
+python mSensor.py -c 31 &
+python mSensor.py -c 41 &
+python mSensor.py -c 51 &
+python mSensor.py -c 61 &
+python mSensor.py -c 71 &
+python mSensor.py -c 81 &
+python mSensor.py -c 91 &
+
