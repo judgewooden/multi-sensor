6af11c5c5cd3a93366d444e0f36a1a3ac86c9d77
judgewooden <douwe.jong@gmail.com>
2016-12-25 21:08:12 +0100

    Latest Version

--- startkastpi.sh
+++ startkastpi.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-export JPH_DEBUG=1
+export JPH_DEBUG=0
 python mSensor.py -c A1 &
 python mSensor.py -c F1 &
-python mSensor.py -c F6 &
 python mSensor.py -c C1 &
+export JPH_DEBUG=1
+python mSensor.py -c F6 &
 
 echo pkill -f mSensor
