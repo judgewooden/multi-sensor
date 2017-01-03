4c6cbdcb8d8265c0ca599c07f5abe6ebb45b3cc6
judgewooden <douwe.jong@gmail.com>
2017-01-03 22:25:17 +0100

    Completed first working Alpha Version

--- startkastpi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export JPH_DEBUG=0
-python sensor.py -c A1 &
-python sensor.py -c F1 &
-python sensor.py -c C1 &
-python sensor.py -c F6 &
-
-echo pkill -f sensor
--- startmad.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export JPH_DEBUG=0
-python sensor.py -c C3 &
-python sensor.py -c X1 &
-python sensor.py -c Y1 &
-
-echo pkill -f sensor
