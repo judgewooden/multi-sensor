fe0da26757519712d8903139244f24b3af1cd5b4
judgewooden <douwe.jong@gmail.com>
2017-08-13 20:30:02 +0200

    Small change to graph

--- jphFlask.py
+++ jphFlask.py
@@ -287,7 +287,7 @@ def sensorboard(codifier=None):
     else:
         timeoutsensor=sensor
     return render_template('sensorboard.html',
-            sensor=sensor, sensors=channel.getAllSensors(), timeoutsensor=timeoutsensor)
+            sensor=sensor, sensors=channel.getAllSensors(), mainSensor=timeoutsensor)
 
 @app.route('/ntp')
 def ntp():
