9c020fd6cf813928075ae4b42c7c8d003b036de0
judgewooden <douwe.jong@gmail.com>
2018-01-27 04:06:57 +0100

    adding a fan controller

--- pytemplates/mixtemp.py
+++ pytemplates/mixtemp.py
@@ -129,18 +129,5 @@ if (xs==False):
             tf={{ M4 }}
             channel.sendData(data=tf, Codifier="PL")
 
-# mad=A4
-# Aqua=A5 
-
-dc=100
-times = [{{ A4|DTimestamp }}, {{ A5|DTimestamp }}]
-l=min(times)
-if (l!=None):
-    if(n-l < 60000):
-        maxtemp = max( [{{ A4 }}, {{ A5 }} ])
-        terror = maxtemp - 37
-        dc={{ FK }} + 1/6 * 5 * terror
-        print(maxtemp, terror, dc, {{ FK }}, {{ A4 }}, {{ A5 }})
-channel.sendCtrl(to="FK", flag="A", timeComponent=dc)
 
 
