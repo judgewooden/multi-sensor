c4f09d49cdc8db97a39705b35f162f81818b39f6
judgewooden <douwe.jong@gmail.com>
2018-01-27 04:59:36 +0100

    adding a fan controller

--- pytemplates/fancontrol.py
+++ pytemplates/fancontrol.py
@@ -27,8 +27,8 @@ l=min(times)
 if (l!=None):
     if(n-l < 60000):
         maxtemp = max( [{{ A4 }}, {{ A5 }} ])
-        terror = maxtemp - 37
+        terror = maxtemp - target
         dc=fk + 1/6 * 5 * terror
-        print(maxtemp, terror, dc, {{ FK }}, {{ A4 }}, {{ A5 }})
+        print(target, maxtemp, terror, dc, fk , {{ A4 }}, {{ A5 }})
 channel.sendData(data=dc, Codifier="FM")
 channel.sendCtrl(to="FK", flag="A", timeComponent=dc)
