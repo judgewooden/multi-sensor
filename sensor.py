7bcbd865092064bad2d9621f8397a248ffb07367
judgewooden <douwe.jong@gmail.com>
2018-03-26 10:55:18 +0200

    use redis for values at restart

--- sensor.py
+++ sensor.py
@@ -336,9 +336,9 @@ class controlSensor(object):
         try:
             self.value = r.hget( Codifier, "Value")
             print("redis  start: ",self.value)
-            if (self.value == None):
+            if (self.value == "None"):
                 self.value=float(channel.getMySensorElement("Default"))
-            print("config start: ",self.value)
+                print("config start: ",self.value)
         except:
             self.value=0
 
